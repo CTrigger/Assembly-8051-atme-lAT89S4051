13dd143c1d3a8f630e3f8aec066e63cc89f32195
Ricardo Kim <hexadecimal.exe@gmail.com>
2015-09-27 03:34:08 -0300

    Update Industrial_Mixer.asm

--- Industrial_Mixer.asm
+++ Industrial_Mixer.asm
@@ -4,136 +4,139 @@
 ;	Brazil, Engineer Student.	10/09/2015
 ;================================================================
 
-buzzer	equ     p0.0    ;Buzzer
+buzzer	equ     p0.0    	;Buzzer
 
-valA	equ	p0.6	;valve A
-valB    equ     p0.5    ;valve B
-valC    equ     p0.4    ;valve C
+valA	equ	p0.6		;valve A
+valB    equ     p0.5    	;valve B
+valC    equ     p0.4    	;valve C
 
-senE	equ     p1.7    ;sensor Empty
-senH	equ     p1.2    ;sensor Half
-senF	equ     p1.1    ;sensor Full
+senE	equ     p1.7    	;sensor Empty
+senH	equ     p1.2    	;sensor Half
+senF	equ     p1.1    	;sensor Full
 
-motor   equ     p0.3    ;Mixer Motor
+motor   equ     p0.3    	;Mixer Motor
 
  
-	ORG     0000H	
-	jmp     preset
+	ORG     0000H		;┐
+	jmp     preset		;┴─Define linha do preset como inicio
 
 
- 	ORG     000BH
-	jmp     delay0
+ 	ORG     000BH		;┐
+	jmp     delay0		;┴─Endereço da interrupção de M00
 
-	ORG	001BH
-	jmp	delay0
+	ORG	001BH		;┐
+	jmp	delay0		;┴─Endereço da interrupção de M10
 
 preset:
+	setb	vala		;┐
+	setb	valb		;┤
+	setb	valc		;┤
+	setb	motor		;┤
+	setb	buzzer		;┴─Define todas as saidas desligadas
 
+	mov     IE,#10001010B	;──Habilita interrupção: EA,ET1,ET0
+	mov     IP,#00000101B	;──Habilita interrupção: PX1,PX0
+	mov     TMOD,#00010001B	;──Habilita timers: M10, M00
 
-	setb	vala
-	setb	valb
-	setb	valc
-	setb	motor
-	setb	buzzer
-;	clr	vala
-;	clr	valb
-;	clr	valc
-;	clr	motor
-;	clr	buzzer
-
-        mov     IE,#10001010B
-        mov     IP,#00000101B
-
-        mov     TMOD,#00010001B
-
-        ljmp    inicio
+	ljmp    inicio		;──Salto para Inicio
  
 
 
 delay0:
 
-        clr     tr0
-        clr	tr1
-        setb    00
-        setb	01
-        reti
+	clr	tr0		;┐
+	clr	tr1		;┴─Limpa TR
+	setb	00		;┤
+	setb	01		;┴─Desabilita funcionamento do t0 e t1
+	reti			;──Retorna da Interrupção
 
 delay1:
-	mov     th0,#0ffH
-        mov     tl0,#000H
-        mov     th1,#0ffH
-        mov     tl1,#0ffH
-        
-	setb    tr0
-	clr	00
-        jnb     00,$
-        
-        setb    tr1
-        clr	01
-        jnb     01,$
-        
-	
-       	ret
+	mov	th0,#000H	;┐
+	mov	tl0,#000H	;┤
+	mov	th1,#000H	;┤
+	mov	tl1,#000H	;┴─Define contador dos timers M10, e M00
+
+	setb    tr0		;┐
+	clr	00		;┤
+        jnb     00,$		;┴─Ativa contagem de M00
+
+        setb    tr1		;┐
+        clr	01		;┤
+        jnb     01,$		;┴─Ativa contagem de M10
 
 
-delay2:
-	mov 	r0,#0FFh
-	mov 	r1,#0FFh
-	mov	r2,#017h
+       	ret			;──Retorna do chamado
 
-AS:	djnz 	r0,$
-	djnz	r1,AS
-	djnz	r2,AS
 
-	ret
 
 inicio:
 
 blocoA:	
-	jnb	senF,erro
-	jnb	senH,Erro
-	jb	senE,BlocoA
-	clr	valA
+	jnb	senF,erro	;┐
+	jnb	senH,Erro	;┤
+	jb	senE,BlocoA	;┴─Está vazio? [001] 
+	
+	clr	valA		;┐
+	setb	valB		;┤
+	setb	valC		;┤
+	setb	motor		;┴─Abre valvula A [10000]
 
 blocoB:
-	jnb	senF,erro
-        jb      senH,BlocoB
-        jnb	senE,erro
-	setb	valA
-        clr	valB
-        clr	motor
+	jnb	senF,erro	;┐
+        jb      senH,BlocoB	;┤
+        jnb	senE,erro	;┴─Está na metade? [010]
+        
+	setb	valC		;┐
+	setb	valA		;┤
+        clr	valB		;┤
+        clr	motor		;┴─Fecha A, Abre B, Liga Motor
+
 
 blocoC:
-	jnb	senE,erro
-	jb	senH,erro
-        jb      senF,blocoC
-       	setb	valB
-        setb	motor
-        clr	valC
+	jnb	senE,erro	;┐
+	jb	senH,erro	;┤
+        jb      senF,blocoC	;┴─Está cheio? [110]
+        
+        setb	valA		;┐
+       	setb	valB		;┤
+        setb	motor		;┤
+        clr	valC		;┴─fecha B, deliga Motor, Abre C
 
 blocoD:
-        jb      senE,blocoD
-        jnb	senF,erro
-	jnb	senH,erro
-	clr	buzzer
-        setb	valC
-        call    delay1
-        setb	buzzer
+        jb      senE,blocoD	;┐
+        jnb	senF,erro	;┤
+	jnb	senH,erro	;┴─Esta vazio? [001]
+	
+	clr	buzzer		;┐
+        setb	valC		;┤
+        setb	valA		;┤
+        setb	valB		;┴─liga alarme sonoro, fecha C
 
-	jmp     inicio
+        mov	r0,#16d		;──carrega 16 voltas
+seg3:	call    delay1		;┐
+        djnz	r0,seg3		;┴─delay aprox 200ms por chamada
+
+        setb	buzzer		;──desliga alarme sonoro
+
+	jmp     inicio		;──volta inicio
 
  erro:
+	setb	valA		;┐
+	setb	valb		;┤
+	setb	valC		;┤
+	setb	motor		;┴─desliga tudo
+	
+	clr	buzzer		;┐
+	call	delay1		;┤
+	setb	buzzer		;┤
+	call	delay1		;┴─alarme sonoro oscilante
 
-	clr	buzzer
-	call	delay1
-	setb	buzzer
-	call	delay1
 
-
- 	jb 	senE,erro
- 	jnb	senH,erro
- 	jnb	senF,erro
- 	jmp	BlocoA
+ 	jb 	senE,erro	;┐
+ 	jnb	senH,erro	;┤
+ 	jnb	senF,erro	;┴─verificado erro e está vazio? [001]
+ 	
+ 	jmp	BlocoA		;──volta para o inicio
  
 
-	end
+	end			;──fim
